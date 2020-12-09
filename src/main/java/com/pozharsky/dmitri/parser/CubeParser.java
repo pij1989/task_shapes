20a5da33bc01f369ae25b7aafc8cefc753e84127
Dimon <dmitri.pozharsky@gmail.com>
2020-12-09 17:11:07 +0300

    done tasks

--- src/main/java/com/pozharsky/dmitri/parser/CubeParser.java
+++ src/main/java/com/pozharsky/dmitri/parser/CubeParser.java
@@ -1,13 +1,34 @@
 package com.pozharsky.dmitri.parser;
 
-import com.pozharsky.dmitri.entity.Point;
+import com.pozharsky.dmitri.validator.CubeInitialDataValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CubeParser {
-    public List<Point> parse(List<String> lines) {
-        List<Point> points = new ArrayList<>();
-        return points;
+    private static final Logger logger = LogManager.getLogger(CubeParser.class);
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_SPACE = " ";
+
+    public List<List<Double>> parse(List<String> lines, CubeInitialDataValidator validator) {
+        List<List<Double>> lists = new ArrayList<>();
+        for (String line : lines) {
+            String[] points = line.split(DELIMITER_COMMA);
+            if (validator.validate(points)) {
+                List<Double> coordinats = Arrays.stream(points)
+                        .flatMap(e -> Stream.of(e.trim().split(DELIMITER_SPACE)))
+                        .map(Double::parseDouble)
+                        .collect(Collectors.toList());
+                lists.add(coordinats);
+            } else {
+                logger.info("Incorrect line: " + line);
+            }
+        }
+        return lists;
     }
 }
--- src/main/java/com/pozharsky/dmitri/validator/CubeInitialDataValidator.java
+++ src/main/java/com/pozharsky/dmitri/validator/CubeInitialDataValidator.java
@@ -1,9 +1,26 @@
 package com.pozharsky.dmitri.validator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.regex.Pattern;
+
 public class CubeInitialDataValidator {
-    public boolean validate(String line){
+    private static final Logger logger = LogManager.getLogger(CubeInitialDataValidator.class);
+    private static final String COORDINAT_REG = "([\\d]+\\.[\\d]+ [\\d]+\\.[\\d]+ [\\d]+\\.[\\d]+)";
 
-
+    public boolean validate(String[] points) {
+        if (points.length < 8) {
+            logger.info("Not enough point with coordinat to create an object of cube. Should be 8 point");
+            return false;
+        }
+        Pattern pattern = Pattern.compile(COORDINAT_REG);
+        for (String point : points) {
+            if (!pattern.matcher(point).matches()) {
+                logger.info("Incorrect coordinat");
+                return false;
+            }
+        }
         return true;
     }
 }
